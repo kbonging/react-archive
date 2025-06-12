bab65dd693d81fe0e937c19c2a730479bdca0b06
bongjung kim <apple75391@gmail.com>
2025-06-12 10:39:03 +0900

    0612 10:38 더보기 버튼 생성 완료

--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -8,18 +8,46 @@ export default function Home() {
   const [popular, setPopular] = useState([]);
   const [nowPlaying, setNowPlaying] = useState([]);
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    fetchMoviesByCategory('popular').then(setPopular).catch(console.error);
-    fetchMoviesByCategory('now_playing').then(setNowPlaying).catch(console.error);
-  }, []);
+    const fetchAll = async () => {
+      setLoading(true);
+
+      try {
+        const [popularData, nowPlayingData] = await Promise.all([
+          fetchMoviesByCategory('popular'),
+          fetchMoviesByCategory('now_playing'),
+        ]);
+
+        setPopular(popularData);
+        setNowPlaying(nowPlayingData);
+      } catch (error) {
+        console.error('영화 데이터를 불러오는 중 오류 발생:', error);
+        // setError("영화 데이터를 불러오는 중 오류가 발생했습니다.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAll();
+}, []);
 
   // console.log(popular);
   // console.log(nowPlaying);
   return (
-    <div className="bg-black text-white min-h-screen">
+    <>
+    {loading ?(
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="w-20 h-20 border-[6px] border-red-600 border-t-transparent rounded-full animate-spin" />
+      </div>
+    ) : (
+      <div className="bg-black text-white min-h-screen">
       <Banner />
       <MovieSet movies={nowPlaying} title="상영중" />
       <MovieSet movies={popular} title="인기 영화" />
-    </div>
+      </div>
+    )}
+    </>
   );
 }
